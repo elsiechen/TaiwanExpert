83f41d65c1fe0223251a184d0135280c04c2c916
elsie <elsiechen915@gmail.com>
2022-06-22 22:12:42 -0400

    Correct name of staticfile storage

--- TaiwanExpert/settings.py
+++ TaiwanExpert/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATIC_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/').replace('\\', '/')
