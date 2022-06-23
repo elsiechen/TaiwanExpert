782db0798ac44b016ed688a684c15259d7234f72
elsie <elsiechen915@gmail.com>
2022-06-22 21:45:01 -0400

    Update default database to use PostgreSQL

--- TaiwanExpert/settings.py
+++ TaiwanExpert/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'TaiwanExpert.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         #'NAME': BASE_DIR / 'db.sqlite3',
         'NAME': env('DATABASE_NAME'),
     }
