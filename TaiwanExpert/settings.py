d3394a233f6bf7a626851a7076eb0bec869dfa95
elsie <elsiechen915@gmail.com>
2022-06-23 22:04:56 -0400

    Correct base dir

--- TaiwanExpert/settings.py
+++ TaiwanExpert/settings.py
@@ -17,8 +17,8 @@ from pathlib import Path
 #BASE_DIR = Path(__file__).resolve().parent.parent
 
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
